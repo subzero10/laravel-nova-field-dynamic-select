569153c5c050716fa245a66c5644bb722a365346
Roy Duineveld <royduineveld@gmail.com>
2019-04-23 12:57:40 +0200

    Show the selected values on the index and detail view instead of the keys

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,16 @@
 <template>
-    <panel-item :field="field" />
+    <panel-item :field="this.dynamicField" />
 </template>
 
 <script>
 export default {
     props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+    computed: {
+        dynamicField() {
+            this.field.value = this.field.options.find(item => item['value'] == this.field.value).label;
+            return this.field;
+        },
+    },
 }
 </script>
